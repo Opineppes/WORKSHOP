4a7e421a20938afe8af46eedd7257b2447b3a167
Opineppes <quentin.legrand.627@gmail.com>
2018-09-12 15:13:18 +0200

    modification des lien js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+var ancienneValeurs = { //map pour stocker les ancienne valeur du profil et les reafficher corectement
+		nom: "",
+		prenom: "",
+		email: "",
+		promo: ""
+};
+
+var baseWebPath = "/";
+
 $("#btn-inscription").click(function(e) {
 	$("#inscription-modal").modal("show");
 	$("#connexion-modal").modal("hide");
@@ -7,11 +16,44 @@ $(".avatar").click(function(e) {
 	$("#avatar").get(0).click();
 });
 
+function onClicModifProfil(e) {
+	$("#control-modifprofil").html("<h6 class=\"text-left\">Commande Utilisateur:</h6>"
+								 + "<div class=\"btn-group\" role=\"group\">"
+								 + "	<button type=\"submit\" class=\"btn btn-primary\" id=\"button-save\">Sauvegarder</button>"
+								 + "	<button class=\"btn btn-secondary\" id=\"button-cancel\">Annuler</button>"
+								 + "</div>");
+
+	ancienneValeurs.nom = $("#profil-Nom").html();
+	ancienneValeurs.prenom = $("#profil-Prenom").html();
+	ancienneValeurs.email = $("#profil-Email").html();
+	
+	$("#profil-Nom").html("<input type=\"text\" name=\"Nom\" class=\"form-control\" value=\"" + ancienneValeurs.nom + "\" />");
+	$("#profil-Prenom").html("<input type=\"text\" name=\"Prenom\" class=\"form-control\" value=\"" + ancienneValeurs.prenom + "\" />");
+	$("#profil-Email").html("<input type=\"email\" name=\"Email\" class=\"form-control\" value=\"" + ancienneValeurs.email + "\" />");
+	
+	$("#button-cancel").click(onClicModifCancel);
+}
+
+function onClicModifCancel(e) {
+	//je remplace les formulaire par les ancienne valeur
+	$("#control-modifprofil").html("<h6 class=\"text-left\">Commande Utilisateur:</h6>"
+								 + "<div class=\"btn-group\" role=\"group\">"
+								 + "	<button class=\"btn btn-info\" id=\"button-modifprofil\" type=\"button\">Modifier Infos</button>"
+								 + "	<button class=\"btn btn-info\" id=\"button-modifpasswd\" type=\"button\" data-toggle=\"modal\" data-target=\"#modifpasswd\">Modifier Mot de passe</button>"
+								 + "</div>");
+	
+	$("#profil-Nom").html(ancienneValeurs.nom);
+	$("#profil-Prenom").html(ancienneValeurs.prenom);
+	$("#profil-Email").html(ancienneValeurs.email);
+	
+	$("#button-modifprofil").click(onClicModifProfil); //je definie l'action executer quand on clique sur l'element avec l'id button-modifprofil que j'ai rajouter avec js
+}
+
 $("#deconnexion").click(function(e) {
 	e.preventDefault();
 	
 	$.post( {
-		url: "/",
+		url: baseWebPath,
 		data: {
 			protocole: "deconnexion"
 		},
@@ -29,7 +71,7 @@ $("#connexion-form").submit(function(e) {
 	e.preventDefault();
 	
 	$.post( {
-		url: "/",
+		url: baseWebPath,
 		data: {
 			protocole: "connexion",
 			email: $("#email-connexion").val(),
@@ -58,7 +100,7 @@ $("#inscription-form").submit(function(e) {
 
 	
 	$.post( {
-		url: "/",
+		url: baseWebPath,
 		data: {
 			protocole: "inscription",
 			email: $("#email").val(),
@@ -91,7 +133,7 @@ $("#avatar").on('change', function(e) {
 	console.log(formData);
 	
 	$.post({
-		url: "/",
+		url: baseWebPath,
 		data: formData,
 		dataType: 'json',
 		processData: false,
