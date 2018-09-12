5e16052c192188b54fe3c24e6cbef169c116f843
Kiineas <wirda436@gmail.com>
2018-09-12 10:39:43 +0200

    ajout requetes sql pour récup annonces par rubrique et par utilisateur

--- PHP/BDD/tables/TableArticle.php
+++ PHP/BDD/tables/TableArticle.php
@@ -2,10 +2,32 @@
 
 class TableArticle extends Table {
 	
+	protected $_getAllByUser ; 
+	protected $_getAllByRubrique ;
+
 	public function __construct()
 	{
 		parent::__construct("article" ,array("id"), array("dateCreation", "infos","emailUtilisateur","nomRubrique"));
+		$this->_getAllByUser = "SELECT * FROM article WHERE emailUtilisateur = :emailUtilisateur";
+		$this->_getAllByRubrique = "SELECT * FROM article WHERE nomRubrique = :nomRubrique";
 	}
+
+	public function getAllByUser($args) {
+		$req = BDD::getBDD()->prepare($this->_getAllByUser);
+			
+		$req->execute($args);
+		
+		return $req->fetchAll();
+	}
+
+	public function getAllByRubrique($args) {
+		$req = BDD::getBDD()->prepare($this->_getAllByRubrique);
+			
+		$req->execute($args);
+		
+		return $req->fetchAll();
+	}
+
 }
 
 ?>
