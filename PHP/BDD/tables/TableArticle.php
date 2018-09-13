01337d6a0cc7ec8be25eb100ecc64013dd6c733f
Kiineas <wirda436@gmail.com>
2018-09-13 16:10:21 +0200

    ajout heure + min pour dates articles

--- PHP/BDD/tables/TableArticle.php
+++ PHP/BDD/tables/TableArticle.php
@@ -9,10 +9,10 @@ class TableArticle extends Table {
 	public function __construct()
 	{
 		parent::__construct("article" ,array("id"), array("titre", "dateCreation", "infos","emailUtilisateur","nomRubrique"));
-		$this->_getAllByUser = "SELECT *, DATE_FORMAT(dateCreation, '%d %M %Y') AS dateInscriptionFormatee FROM article WHERE emailUtilisateur = :emailUtilisateur ORDER BY dateCreation DESC";
-		$this->_getAllByRubrique = "SELECT *, DATE_FORMAT(dateCreation, '%d %M %Y') AS dateInscriptionFormatee  FROM article WHERE nomRubrique = :nomRubrique ORDER BY dateCreation DESC";
-		$this->_selectOne = "SELECT *, DATE_FORMAT(dateCreation, '%d %M %Y') AS dateInscriptionFormatee FROM article WHERE id = :id ";
-		$this->_search = "SELECT *, DATE_FORMAT(dateCreation, '%d %M %Y') AS dateInscriptionFormatee FROM article WHERE nomRubrique = :nomRubrique AND (titre LIKE :test OR infos LIKE :test OR emailUtilisateur LIKE :test) ORDER BY dateCreation DESC";
+		$this->_getAllByUser = "SELECT *, DATE_FORMAT(dateCreation, ' %d/%m/%Y à %H:%i') AS dateInscriptionFormatee FROM article WHERE emailUtilisateur = :emailUtilisateur ORDER BY dateCreation DESC";
+		$this->_getAllByRubrique = "SELECT *, DATE_FORMAT(dateCreation, ' %d/%m/%Y à %H:%i') AS dateInscriptionFormatee  FROM article WHERE nomRubrique = :nomRubrique ORDER BY dateCreation DESC";
+		$this->_selectOne = "SELECT *, DATE_FORMAT(dateCreation, ' %d/%m/%Y à %H:%i') AS dateInscriptionFormatee FROM article WHERE id = :id ";
+		$this->_search = "SELECT *, DATE_FORMAT(dateCreation, ' %d/%m/%Y à %H:%i') AS dateInscriptionFormatee FROM article WHERE nomRubrique = :nomRubrique AND (titre LIKE :test OR infos LIKE :test OR emailUtilisateur LIKE :test) ORDER BY dateCreation DESC";
 	}
 
 	public function getAllByUser($args) {
